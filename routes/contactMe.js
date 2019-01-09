17aa17e9c0be2cd8d04482712af1385c8a1651d0
ssaifali <ssaifali@in.ibm.com>
2019-01-10 03:15:42 +0530

    minor fix

--- routes/contactMe.js
+++ routes/contactMe.js
@@ -19,11 +19,11 @@ router.post('/',function (req,res,next) {
         // console.log("contact:",contact);
         if(err){
             console.log("error happened!");
-            // res.json({success:false,msg:'failed to submit feedback/query'});
-            res.json(false);
+            res.json({success:false,msg:'failed to submit feedback/query'});
+            // res.json(false);
         }else {
-            // res.json({success:true,msg:'feedback/query submitted'})
-            res.json(true);
+            res.json({success:true,msg:'feedback/query submitted'})
+            // res.json(true)
         }
     });
 
