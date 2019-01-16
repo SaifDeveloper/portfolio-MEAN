f9e30b5f0722ed8c431d51b93d90778ea14e5cf4
ssaifali <ssaifali@in.ibm.com>
2019-01-16 18:12:08 +0530

    certificate links added

--- frontend/src/app/certifications/certifications.component.ts
+++ frontend/src/app/certifications/certifications.component.ts
@@ -14,9 +14,24 @@ export class CertificationsComponent implements OnInit {
   ngOnInit() {
   }
 
-  verify(){
-    // this.router.navigate(['https://www.google.com'])
-    window.location.href = "https://www.google.com";
+  verify(certificate){
+    switch(certificate){
+      case 'buildDapps':{
+          window.open("https://www.udemy.com/certificate/UC-B2Z3I1OJ/", "_blank");
+      }
+      case 'Udemy_sg_Eth':{
+        window.open("https://www.udemy.com/certificate/UC-2W6TP01I/", "_blank");
+      }
+      case 'Udemy_sg_GO':{
+        window.open("https://www.udemy.com/certificate/UC-ABAVBWFX/", "_blank");
+      }
+      case 'coursera_ibm_iot':{
+        window.open("https://www.coursera.org/account/accomplishments/verify/NZ4W9D6RUPEQ", "_blank");
+      }
+      case 'py_coursera':{
+        window.open("https://github.com/SaifDeveloper/react-docker", "_blank");
+      }
+    }
   }
 
 }
